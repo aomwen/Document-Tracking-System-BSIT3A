986ca9a9b072085c9c6f0dc142e74880c483502f
nightcroww <franz_paragas@yahoo.com>
2017-10-01 04:27:31 +0800

    case sensitive login

--- application/models/users_model.php
+++ application/models/users_model.php
@@ -31,8 +31,8 @@ class Users_model extends CI_Model {
 	}
 
 	public function login($username,$password){
-		$this->db->where("username",$username);
-		$this->db->where("password",$password);
+		$this->db->where("username like binary",$username);
+		$this->db->where("password like binary",$password);
 		$query=$this->db->get("users");
 		if($query->num_rows()>0){
 		return true;
