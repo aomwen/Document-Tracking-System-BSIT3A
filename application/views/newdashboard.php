0e7feda00504e4fa6c0b17a4e6d0d7817978202e
omgiane <gianeenoda@gmail.com>
2017-10-01 03:14:44 -0700

    Homepage responsive

--- application/views/newdashboard.php
+++ application/views/newdashboard.php
@@ -3,59 +3,77 @@
 	<title>Home</title>
 	<link href="<?php echo base_url('assets/css/newdashboardstyle.css'); ?>" rel="stylesheet" />
 	<link href="<?php echo base_url('assets/css/font-awesome.css'); ?>" rel="stylesheet" />
+	<script type="text/javascript">
+		function myFunction() {
+		    var x = document.getElementById("myTopnav");
+		    if (x.className === "topnav") {
+		        x.className += " responsive";
+		    } else {
+		        x.className = "topnav";
+		    }
+		}
+</script>
 </head>
 <body>
 	<!-- Navigation Bar -->
+<div class="row" style="box-sizing: border-box;">
 <nav class="navbar-fixed-top" id="header">
 	<header id="header_outer" >	
 	  	<div class="container">
 	    	<div class="header_section">
-	      		<div class="logo">
-	      			<a href="javascript:void(0)" data-target="#myNavbar"><img src="<?php echo base_url('assets/images/logoname2.png'); ?>" alt="Document Tracking System"></a>
-	      		</div>
-      			<nav class="nav" id="nav">
-        			<ul class="toggle">
-          				<li><a href="#">Home</a></li>
-          				<li><a href="#services">Services</a></li>
-          				<li><a href="#offices">Offices</a></li>
-          				<li><a href="#contact">Contact</a></li>
-          				<li><a href="<?php echo base_url('Access/log_in');?>">Log In</a></li>
-          				<li><a href="<?php echo base_url('Access/signup');?>">Sign Up</a></li>
-        			</ul>
-       	 			<ul class="">
-          				<li><a href="#">Home</a></li>
-          				<li><a href="#service">Services</a></li>
-          				<li><a href="#contact">Contact</a></li>
-          				<li><a href="<?php echo base_url('Access/log_in');?>">Log In</a></li>
-          				<li><a href="<?php echo base_url('Access/signup');?>">Sign Up</a></li>
-        			</ul>
-      			</nav>
+	    		<div class="topnav" id="myTopnav">
+  					<a class="col-sm-4" href="#" style="float:left;">
+  						<img src="<?php echo base_url('assets/images/logoname2.png'); ?>" alt="Document Tracking System" style="max-width:100%; height:auto;">
+  					</a>
+  					<a id="drop" href="#">Home</a></li>
+  					<a id="drop" href="#service">Services</a>
+  					<a id="drop" href="#contact">Contact</a>
+      				<a id="drop" href="<?php echo base_url('Access/log_in');?>">Log In</a>
+      				<a href="javascript:void(0);" style="font-size:16px;" class="icon" onclick="myFunction()">&#9776;</a>
+				</div>
 	  		</div>
 	  	</div>
 	</header>
 </nav>
+</div>
+<!-- <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+          
+      		<div class="navbar-header page-scroll">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="navbar-main-collapse" >
+	            	<span class="icon-bar"></span>
+	              	<span class="icon-bar"></span>
+	              	<span class="icon-bar"></span> 
+              </button>
+              <a class="navbar-brand" href="#"><h1>Shaarii Shaarii Store</h1></a>
+        			
+      		</div>
+          
+      		
+          <div class="collapse navbar-collapse navbar-main-collapse" >
+        		<ul class="nav navbar-nav navbar-right">
+          			<li class="dropdown">
+           			 	<a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-user"></span> Account
+            			<span class="caret"></span></a>
+            			<ul class="dropdown-menu">
+              				<li ><a href="#" class="list"><span class="glyphicon glyphicon-cog"></span> Account Settings</a></li>
 
-	<!-- Carousel -->
-	<!-- <div class="jumbotron" id="top_content" style="background: url('assets/images/login.jpg') no-repeat; background-position: cover;">
-		<h2 > YOUR DOCUMENT, LET'S TRACK IT!</h2>
-		<form class="form-inline" style="">
-			<div class="input-group">
-				<input type="text" class="form-control" size="70" placeholder="Track Code" required />
-					<div class="input-group-btn">
-						<button type="button" class="btn btn-info"><span class="glyphicon glyphicon-search"></span></button>
-					</div>
-			</div>
-		</form>  	
-	</div> -->
-		<div class="carousel slide" data-ride="carousel" id="top_content" style="padding-top: 5%;">
+              				<li ><a href="<?php echo base_url('logout'); ?>"class="list"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+            			</ul>
+          			</li>
+          			<li><a href="#"><span class="glyphicon glyphicon-bell"></span> Notification</a></li>
+          			<li><a href="#"><span class="glyphicon glyphicon-question-sign"></span> Help</a></li>
+        		</ul>
+          </div>
+    	</div>
+</nav> -->
+		<div class="carousel slide" data-ride="carousel" id="top_content">
 			<ol class="carousel-indicators">
 				<li data-target="#myCarousel" data-slide-to="1" class="active"></li>
-				<!-- <li data-target="#myCarousel" data-slide-to="2"></li>
-				<li data-target="#myCarousel" data-slide-to="3"></li> -->
 			</ol>
 			<div class="carousel-inner" role="listbox">
-				<div class="carousel-caption" style="padding-bottom:25%;">
-					<h2 style="font-family: Verdana;"> YOUR DOCUMENT, LET'S TRACK IT!</h2>
+				<div class="carousel-caption" id="track">
+					<h2 class="h2style" id="h2" style="font-family: Verdana;"> YOUR DOCUMENT, LET'S TRACK IT!</h2>
 					<form class="form-inline" >
 						<div class="input-group">
 							<input type="text" class="form-control" size="70" placeholder="Track Code" required />
@@ -66,47 +84,35 @@
 					</form>  
 				</div>				
 				<div class="item active" id="slide1">
-					<img src="<?php echo base_url('assets/images/signup.jpeg');?>" alt="office" height="100%" width="100%">
+					<img src="<?php echo base_url('assets/images/signup.jpeg');?>" alt="office" style="width:100%; height:auto;">
 				</div>
-				<!-- <div class="item" id="slide2">
-					<img src="<?php echo base_url('assets/images/pexels-photo-5.png');?>" alt="office">				
-				</div>  
-				<div class="item" id="slide3">
-			 		<img src="<?php echo base_url('assets/images/pexels-photo-3.png');?>" alt="office">
-				</div> -->
-				<!-- <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-					<span class="sr-only">Previous</span>
-				</a>
-				<a class="right carousel-control" href="#myCarousel" data-slide="next"></a>
-					<span class="sr-only">Next</span>
-				</a> -->
 			</div>
 		</div>
 	<!-- Services -->
 	<section id="service">
 		<div class="container">
-			<h2> Services </h2>
+			<h2 class="h2style"> Services </h2>
 			<div class="service_area">
 				<div class="row">
 					<div class="col-lg-4">
 						<div class="service_block">
 							<img src="<?php echo base_url('assets/images/document-icon-1.png');?>" alt="Features" width="200px" height="200px" />
-							<h3>Powerful Features</h3>
-							<p>Automate time consuming task like going to the university just to acquire the documents needed with just a few clicks</p>
+							<h3 class="h3style">Powerful Features</h3>
+							<p class="pstyle">Automate time consuming task like going to the university just to acquire the documents needed with just a few clicks</p>
 						</div>
 					</div>
 					<div class="col-lg-4">
 						<div class="service_block">
 							<img src="<?php echo base_url('assets/images/document-icon-2.png');?>" alt="New York" width="200px" height="200px"/>
-							<h3>Highly Secured</h3>
-							<p>This Websites provides a high level of documents encryption and role based access</p>
+							<h3 class="h3style">Highly Secured</h3>
+							<p class="pstyle">This Websites provides a high level of documents encryption and role based access</p>
 						</div>
 					</div>
 					<div class="col-lg-4">
 						<div class="service_block">
 							<img src="<?php echo base_url('assets/images/document-icon-3.png');?>" alt="San Francisco" width="200px" height="200px">
-							<h3>User-friendly</h3>
-							<p>TUP Document Tracking System makes things easy and convenient for the users</p>
+							<h3 class="h3style">User-friendly</h3>
+							<p class="pstyle">TUP Document Tracking System makes things easy and convenient for the users</p>
 						</div>
 					</div>
 				</div>				
@@ -114,22 +120,22 @@
 					<div class="col-lg-4">
 						<div class="service_block">
 							<img src="<?php echo base_url('assets/images/document-icon-4.png');?>" alt="" height="200px" width="200px"/>
-							<h3>Record Management</h3>
-							<p>Allows user to track, view and transfer documents to any offices/departments within the University.</p>
+							<h3 class="h3style">Record Management</h3>
+							<p class="pstyle">Allows user to track, view and transfer documents to any offices/departments within the University.</p>
 						</div>
 					</div>
 					<div class="col-lg-4">
 						<div class="service_block">
 							<img src="<?php echo base_url('assets/images/document-icon-5.png');?>" alt="" height="200px" width="200px"/>
-							<h3>Document review and control</h3>
-							<p>Enjoy complete visibility into document review process and get control over documents.</p>
+							<h3 class="h3style">Document review and control</h3>
+							<p class="pstyle">Enjoy complete visibility into document review process and get control over documents.</p>
 						</div>
 					</div>
 					<div class="col-lg-4">
 						<div class="service_block">
 							<img src="<?php echo base_url('assets/images/document-icon-6.png');?>" alt="" height="200px" width="200px"/>
-							<h3>Convenient Access</h3>
-							<p>Gain convenient access to documents of any type and of any offices/departments.</p>	
+							<h3 class="h3style">Convenient Access</h3>
+							<p class="pstyle">Gain convenient access to documents of any type and of any offices/departments.</p>	
 						</div>
 					</div>								
 				</div>
@@ -140,7 +146,7 @@
 		<div class="container">
 			<section class="main-section contact" id="contact">
 				<div class="contact_section">
-					<h2>Contact</h2>
+					<h2 class="h2style">Contact</h2>
 					<div class="row">
 						<div class="col-lg-4">
 							<div class="contact_block">
@@ -181,22 +187,18 @@
 				<div class="row">
 					<div class="col-lg-6">
 						<div class="contact-info-box address clearfix">
-							<h3>Need something? Drop a comment!</h3>
-							<p>Accusantium quam, aliquam ultricies eget tempor id, aliquam eget nibh et. Maecen aliquam, risus at semper. Accusantium quam, aliquam ultricies eget tempor id, aliquam eget nibh et. Maecen aliquam, risus at semper.</p>
-            				<p>Accusantium quam, aliquam ultricies eget tempor id, aliquam eget nibh et. Maecen aliquampor id.</p>
+							<h3 class="h3style">Need something? Drop a comment!</h3>
+							<p class="pstyle">Accusantium quam, aliquam ultricies eget tempor id, aliquam eget nibh et. Maecen aliquam, risus at semper. Accusantium quam, aliquam ultricies eget tempor id, aliquam eget nibh et. Maecen aliquam, risus at semper.</p>
+            				<p class="pstyle">Accusantium quam, aliquam ultricies eget tempor id, aliquam eget nibh et. Maecen aliquampor id.</p>
 						</div>
-						<!-- <ul class="social-link">
-							<li class="facebook"><a href=""><i class="fa-facebook"></i></a></li>
-							<li class="twitter"><a href="javascript:void(0)"><i class="fa-twitter"></i></a></li>
-						</ul> -->
 					</div>
 					<div class="col-lg-6">
-						<div class="form">
-							<input class="input-text" type="text" name="name" placeholder="Name">
+						<form class="form" method="POST" action="<?php echo base_url('HomeFunction/msgtoAdmin');?>">
+							<input class="input-text" type="text" name="author" placeholder="Name">
 							<input class="input-text" type="text" name="email" placeholder="Email">
-							<textarea class="input-text text-area" name="comment" cols="0" rows="0" placeholder="Comment"></textarea>
+							<textarea class="input-text text-area" name="content" cols="0" rows="0" placeholder="Comment"></textarea>
 							<input class="input-btn" type="submit" value="Send">
-						</div>
+						</form>
 					</div>
 				</div>
 			</section>
@@ -207,7 +209,7 @@
 			</div>
 		</div>
 	</footer>
-		<script>
+<!-- <script>
 $(document).ready(function(){
   // Add smooth scrolling to all links in navbar + footer link
   $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
@@ -233,6 +235,7 @@ $(document).ready(function(){
     } // End if
   });
 })
-</script>
+</script> -->
+
 </body>
 </html>
