16915abb73b8c7af5517a0a8d0e355770fbfdc3a
omgiane <gianeenoda@gmail.com>
2017-10-16 15:58:19 -0700

    Guide

---
+++ application/controllers/Guide.php
@@ -0,0 +1,44 @@
+<?php 
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Guide extends CI_Controller {
+
+
+
+    public function __construct(){
+        parent::__construct();
+        $this->load->model('documentsModel','Files');
+        $this->load->model('usersModel','User');
+        if(!isset($_SESSION['username']))
+        {
+            redirect().'Dts/index';
+        }
+    }
+    public function guide()
+    {
+        $data['title'] = "Document Tracking System - Dashboard";
+
+        $user = $this->session->userdata('username');
+        $condition = array('username' => $user);
+        $userdata = $this->User->read($condition);
+        $data['userdata'] = $userdata;
+
+        $condition = null;
+        $documents = $this->Files->read($condition);
+        $data['documents']=$documents;
+        
+        $this->load->view('include/header',$data);  
+        if($_SESSION['username'] == "admin"){
+            $this->load->view('include/header',$data);    
+            $this->load->view('profileAdmin',$data);
+        }else{
+            $this->load->view('include/header',$data);
+            $this->load->view('profile',$data);
+        }
+        $this->load->view('guide',$data);
+    }
+
+    
+}
+?>
