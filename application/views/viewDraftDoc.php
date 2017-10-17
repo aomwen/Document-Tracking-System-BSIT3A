1cd7a6a67747f06d6c65d48a465d804a783e01ba
AbbieOas <oasabbie@gmail.com>
2017-10-17 16:24:11 +0800

    tinanggal yung panel heading 3

--- application/views/viewDraftDoc.php
+++ application/views/viewDraftDoc.php
@@ -1,73 +1,16 @@
 <!--My Documents --> 
 <head>
-	<style>
-		.breadcrumb{
-		  margin-top:10px;
-		}
-		.myinbox{
-			margin-top: 75px;	
-			margin-left: 20%;
-			width:79%;
-			height:100%;
-		}
-		#head{
-		  border-bottom:solid #015249;
-		}
-		.panel-heading h3{
-		  color:#015249;
-		}
-		.panel-heading ol li a span{
-		  color:#015249;
-		}
-		.panel-body form input{
-			padding:15px 16px;
-			border:1px solid #ccc;
-			border-radius:4px;
-			font-size:15px;
-			color:#aaa;
-			font-family: 'Lato', sans-serif;
-		}
-		.panel-body form button{
-			background:#015249;
-			color:#fff;
-			width:40px;
-		}
-		.panel-body form button:hover{
-			background:#A5A5AF;
-			color:#222;
-		}
-		.searchbar{
-			display:inline-flex;
-			height: 35px;
-		}
-		.search{
-			width:400px;
-			margin-left: 15px;
-		}
-		#collapse a{
-			text-decoration: none;
-		}
-		.panel-group{
-			background-color: #fff;
-		}
-		.inboxto{
-			border-bottom: 1px solid #dadada;
-		}
-		.inboxto a{
-			text-decoration:none; 
-		}
-	</style>	
 	<link href="<?php echo base_url('bootstrap/css/Staff-Designs.css'); ?>" rel="stylesheet" />
 </head>
 <div class="myinbox col-md-9">
-	<div class="panel panel-default">
-		<div class="panel-heading" id="head">
-		    <ol class="breadcrumb pull-right">
-		      <li><a href="<?php echo base_url('DocumentStatus/viewDocuments'); ?>"><span class="glyphicon glyphicon-home"></span></a></li> 
-		      <li class="active">Drafts</li>
-		    </ol>    
-		    <h3><span class="glyphicon glyphicon-inbox"></span> Drafts</h3>       
-		</div>
+	<div class="panel-heading" id="head">
+	    <ol class="breadcrumb pull-right">
+	      <li><a href="<?php echo base_url('DocumentStatus/viewDocuments'); ?>"><span class="glyphicon glyphicon-home"></span></a></li> 
+	      <li class="active">Drafts</li>
+	    </ol>    
+	    <h3><span class="glyphicon glyphicon-inbox"></span> Drafts</h3>       
+	</div>
+	<div class="panel panel-default">		
 		<div class="panel-body">
 			<input type="text" onkeyup="FilterFunction()" placeholder=" e.g. 592-***-**" id="myInputDocumentSearch" class="search"/>
 				<button><a type="submit"  href="<?php base_url('DocumentDraft/viewDraft')?>"><span class="glyphicon glyphicon-repeat"></span></a></button>
