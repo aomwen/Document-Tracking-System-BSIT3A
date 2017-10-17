dd7b3f27a43489cb36a1febdaf379a6150332b37
AbbieOas <oasabbie@gmail.com>
2017-10-18 01:52:41 +0800

    Responsive admin 3

--- application/views/viewSentDoc.php
+++ application/views/viewSentDoc.php
@@ -1,59 +1,64 @@
 <head>
-<link href="<?php echo base_url('bootstrap/css/Staff-Designs.css'); ?>" rel="stylesheet" />
+	<link href="<?php echo base_url('bootstrap/css/Staff-Designs.css'); ?>" rel="stylesheet" />
 </head>
-<div class="mysent col-md-9">
-	<div class="panel-heading" id="head">
-	    <ol class="breadcrumb pull-right">
-	      <li><a href="<?php echo base_url('DocumentStatus/viewDocuments'); ?>" title="Home"><span class="glyphicon glyphicon-home"></span></a></li> 
-	      <li class="active">Sent Documents</li>
-	    </ol>    
-	    <h3><span class="glyphicon glyphicon-folder-open"></span> Sent Documents</h3>       
-	</div>
-	<div class="panel panel-default">		
-		<div class="panel-body">
-			<form class="pull-right searchbar">
-				<input type="text" id="myInputDocumentSearch" onkeyup="FilterFunction()" placeholder=" e.g. 592-***-**" name="tracknumber" class="search"/>
-				<a  class="btn" href="<?php base_url('DocumentStatus/mydocuments_view')?>" title="Search"><span class="glyphicon glyphicon-repeat"></span></a>
-			</form>
-		</div>        
-		<div class="table-responsive">
-	        <table id="myTable" class="docstatus table-bordered table-hover table-striped table-center text-center" width="100%">
-	        	<tr>
-	        		<th>Tracknumber</th>
-	        		<th>Filename</th>
-	        		<th>Receiver</th>
-	        		<th>Date Received</th>
-	        	</tr>
-	          	<?php
-	          		$thereis=false;
-	         		foreach ($documents as $d){
-						if($d['sender']==$_SESSION['username']&&$d['sentDelete']==FALSE){
-			        		if($d['seen']==FALSE){
-			        			echo '
-			        				<tr style="background-color: #f9f9f9;">';
-			        		}else{
-			        			echo '
-			        				<tr >';
-			        		}
-			        		echo'
-			            		<td><a href="'.base_url('DocumentSent/viewSentMess/'.$d['trackcode'].'').'"><b>'.$d['trackcode'].'</b></td>
-			            		<td>'.$d['filename'].'</a></td>
-			            		<td>'.$d['sender'].'</td>
-			            		<td>'.$d['datecreated'].'</td>
-			        		</tr>';
-			        // <td><span class="glyphicon glyphicon-paperclip"></span></td>
-			            	$thereis=true;
-			    		}
-			    	}
-			    	if($thereis==false){
-						echo '<tr><td colspan="4" class="text-danger" align="center">No document sent...</td></tr>';
-					}
-	          	?> 
-	        </table>
-	      </div>
-	</div>	
-	</div>			
-</div>
+	<div col-xs-9 col-sm-9 col-md-9 col-lg-9">
+		    <div id="content">
+		      	<div id="main-content">
+					<div class="panel-heading" id="head">
+					    <ol class="breadcrumb pull-right">
+					      	<li><a href="<?php echo base_url('DocumentStatus/viewDocuments'); ?>" title="Home"><span class="glyphicon glyphicon-home"></span></a></li> 
+					      	<li class="active">Sent Documents</li>
+					    </ol>    
+					    <h3><span class="glyphicon glyphicon-folder-open"></span> Sent Documents</h3>       
+					</div>
+					<div class="panel panel-default">		
+						<div class="panel-body">
+							<form class="pull-right searchbar">
+								<input type="text" id="myInputDocumentSearch" onkeyup="FilterFunction()" placeholder=" e.g. 592-***-**" name="tracknumber" class="search"/>
+								<a  class="btn" href="<?php base_url('DocumentStatus/mydocuments_view')?>" title="Search"><span class="glyphicon glyphicon-repeat"></span></a>
+							</form>  
+						</div>	      
+						<div class="table-responsive">
+					        <table id="myTable" class="docstatus table-bordered table-hover table-striped table-center text-center" width="100%">
+					        	<tr>
+					        		<th>Tracknumber</th>
+					        		<th>Filename</th>
+					        		<th>Receiver</th>
+					        		<th>Date Received</th>
+					        	</tr>
+					          	<?php
+					          		$thereis=false;
+					         		foreach ($documents as $d){
+										if($d['sender']==$_SESSION['username']&&$d['sentDelete']==FALSE){
+							        		if($d['seen']==FALSE){
+							        			echo '
+							        				<tr style="background-color: #f9f9f9;">';
+							        		}else{
+							        			echo '
+							        				<tr >';
+							        		}
+							        		echo'
+							            		<td><a href="'.base_url('DocumentSent/viewSentMess/'.$d['trackcode'].'').'"><b>'.$d['trackcode'].'</b></td>
+							            		<td>'.$d['filename'].'</a></td>
+							            		<td>'.$d['sender'].'</td>
+							            		<td>'.$d['datecreated'].'</td>
+							        		</tr>';
+							        // <td><span class="glyphicon glyphicon-paperclip"></span></td>
+							            	$thereis=true;
+							    		}
+							    	}
+							    	if($thereis==false){
+										echo '<tr><td colspan="4" class="text-danger" align="center">No document sent...</td></tr>';
+									}
+					          	?> 
+					        </table>
+					    </div>
+					</div>
+			    </div>    
+			</div>    
+	    </div>
+	</div> 
+</div>	
 <script>
 function FilterFunction() {
   var input, filter, table, tr, td, i, x,flag;
