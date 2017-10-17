b3332172b4ee9cbf6472d8727c78182cb0af3a44
AbbieOas <oasabbie@gmail.com>
2017-10-17 16:22:10 +0800

    tinanggal yung panel heading 2

--- application/views/officesContent.php
+++ application/views/officesContent.php
@@ -1,10 +1,15 @@
 <link href="<?php echo base_url('bootstrap/css/Staff-Designs.css'); ?>" rel="stylesheet" />
 
 <div class="myinbox col-md-9 pull-right">
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<?php foreach($colleges as $s){echo $s['collegefull'];}?>
-		</div>
+	<div class="panel-heading" id="head">
+	    <ol class="breadcrumb pull-right">
+	      <li><a href="<?php echo base_url('DocumentStatus/viewDocuments'); ?>"><span class="glyphicon glyphicon-home"></span></a></li> 
+	      <li class="active">Offices</li>
+	    </ol>    
+	    <h3><span class="glyphicon glyphicon-inbox"></span><?php foreach($colleges as $s){echo $s['collegefull'];}?></h3>       
+	</div>
+	
+	<div class="panel panel-default">	
 		<div class="panel-body">
 			<div class="panel-group" id="accordion">
 				<div class="panel panel-default">
