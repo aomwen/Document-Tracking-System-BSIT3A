f273aa3a170446a4ac0ea243bc7413a99adbc76a
AbbieOas <oasabbie@gmail.com>
2017-10-17 16:25:37 +0800

    tinanggal yung panel heading 4

--- application/views/viewSentMess.php
+++ application/views/viewSentMess.php
@@ -2,15 +2,15 @@
 	<link href="<?php echo base_url('bootstrap/css/Staff-Designs.css'); ?>" rel="stylesheet" />
 </head>
 <div class="mysent col-md-9">
-	<div class="panel panel-default"> 
-		<div class="panel-heading" id="head">
-		    <ol class="breadcrumb pull-right">
-		      <li><a href="<?php echo base_url('DocumentStatus/viewDocuments'); ?>"><span class="glyphicon glyphicon-home"></span></a></li> 
-		      <li><a href="<?php echo base_url('DocumentSent/viewSentDocu'); ?>">Sent</a></li> 		      
-		      <li class="active">Track Number</li>
-		    </ol>    
-		    <h3><span class="glyphicon glyphicon-folder-open"></span> Sent</h3>       
-		</div>
+	<div class="panel-heading" id="head">
+	    <ol class="breadcrumb pull-right">
+	      <li><a href="<?php echo base_url('DocumentStatus/viewDocuments'); ?>"><span class="glyphicon glyphicon-home"></span></a></li> 
+	      <li><a href="<?php echo base_url('DocumentSent/viewSentDocu'); ?>">Sent</a></li> 		      
+	      <li class="active">Track Number</li>
+	    </ol>    
+	    <h3><span class="glyphicon glyphicon-folder-open"></span> Sent</h3>       
+	</div>
+	<div class="panel panel-default"> 	
 		<div class="panel-body">
 			<?php
 			foreach($documents as $d){
