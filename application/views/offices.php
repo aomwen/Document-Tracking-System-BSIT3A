469501783794cd2ec36fc296e0bb4c3d5223f3e5
AbbieOas <oasabbie@gmail.com>
2017-10-18 03:13:18 +0800

    Offices responsive

--- application/views/offices.php
+++ application/views/offices.php
@@ -1,35 +1,42 @@
-<link href="<?php echo base_url('bootstrap/css/Staff-Designs.css'); ?>" rel="stylesheet" />
-
-<div class="offices col-md-9">
-	<div class="panel-heading" id="head">
-	    <ol class="breadcrumb pull-right">
-	      <li><a href="<?php echo base_url('DocumentStatus/viewDocuments'); ?>" title="Home"><span class="glyphicon glyphicon-home"></span></a></li> 
-	      <li class="active">Offices</li>
-	    </ol>    
-	    <h3><span class="glyphicon glyphicon-map-marker"></span> Offices</h3>       
-	</div>
-	<div class="panel panel-default">		
-		<div class="panel-body"> 
-			<?php
-				$thereis=false;
-				foreach($colleges as $col){
-					echo'
-					<div class="thumbnail text-center col-sm-3" style="background-color:lightgray; border-width: 2px;  height:25%; padding:10px;">
-						<a href="'.base_url('Office/officeContent/'.$col['collegeId']).'" style="text-decoration: none;">
-							<div style="color: black;">
-								<img class="img-responsive" src="'.$col['collegeLogo'].'" style=" height: 150px;  margin: auto; border-radius: 90%;">
-								<hr style="width:80%;"  />
-								<h4 class="text-primary">'.$col['collegefull'].' ('.$col['collegeId'].')</h4>
-								'.$col['collegeDesc'].'<br />
-							</div>
-						</a>
-					</div>';
-					$thereis=true;
-				}
-				if($thereis==false){
-						echo '<h4 class="text-danger">No college registered...</h4>';
-					}
-			?>
-		</div>	
-	</div>	
+<head>
+	<link href="<?php echo base_url('bootstrap/css/Staff-Designs.css'); ?>" rel="stylesheet" />
+</head>
+		<div col-xs-9 col-sm-9 col-md-9 col-lg-9">
+		    <div id="content">
+		      	<div id="main-content">
+					<div class="panel-heading" id="head">
+					    <ol class="breadcrumb pull-right">
+					    	<li><a href="<?php echo base_url('DocumentStatus/viewDocuments'); ?>" title="Home"><span class="glyphicon glyphicon-home"></span></a></li> 
+					      	<li class="active">Offices</li>
+					    </ol>    
+					    <h3><span class="glyphicon glyphicon-map-marker"></span> Offices</h3>       
+					</div>
+					<div class="panel panel-default">		
+						<div class="panel-body">
+							<?php
+							$thereis=false;
+							foreach($colleges as $col){
+								echo'
+								<div class="thumbnail text-center col-sm-3" style="background-color:lightgray; border-width: 2px;  height:25%; padding:10px;">
+									<a href="'.base_url('Office/officeContent/'.$col['collegeId']).'" style="text-decoration: none;">
+										<div style="color: black;">
+											<img class="img-responsive" src="'.$col['collegeLogo'].'" style=" height: 150px;  margin: auto; border-radius: 90%;">
+											<hr style="width:80%;"  />
+											<h4 class="text-primary">'.$col['collegefull'].' ('.$col['collegeId'].')</h4>
+											'.$col['collegeDesc'].'<br />
+										</div>
+									</a>
+								</div>';
+								$thereis=true;
+							}
+							if($thereis==false){
+									echo '<h4 class="text-danger">No college registered...</h4>';
+								}
+							?>
+						</div>	
+					</div>
+			    </div>    
+			</div>    
+	    </div>
+	</div> 
 </div>	
--- application/views/officesContent.php
+++ application/views/officesContent.php
@@ -1,38 +1,49 @@
 <head>
-	<link href="<?php echo base_url('assets/css/guide.css'); ?>" rel="stylesheet" />
 	<link href="<?php echo base_url('bootstrap/css/Staff-Designs.css'); ?>" rel="stylesheet" />
 </head>
-<div class="myinbox col-md-9 pull-right">
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<?php foreach($colleges as $s){
-				echo '<h3>'.$s['collegefull'].'</h3>';
-			}?>
+		<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+		    <div id="content">
+		      	<div id="main-content">
+		      		<div class="panel-heading" id="head">
+		      			<ol class=" breadcrumb pull-right">
+		      				<li><a href="<?php echo base_url('DocumentStatus/viewDocuments'); ?>" title="Home"><span class="glyphicon glyphicon-home"></span></a></li>
+		      				<li class="Offices"></li>
+		      			</ol>
+		      			<h3><span class="glyphicon glyphicon-map-marker"></span>
+		      				<?php foreach($colleges as $s){
+								echo '<h3>'.$s['collegefull'].'</h3>';
+							}?>
+		      			</h3>
+		      		</div>
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<?php
+							$thereis=false;
+							foreach($departments as $d){
+								$dept = str_ireplace(' ','-',$d['department']);
+								echo'
+								<button class="accordion">'.$d['department'].'</button>
+								<div class="panel" id="panel">
+									<p>'.$d['departmentHead'].'<b>(head)</b></p>';
+									foreach($userdata as $us){
+										if($us['department'] == $d['department']){
+											echo '<p>'.$us['firstname'].' '.$us['lastname'].'<b>'.$us['position'].'</b></p>';
+										}
+										echo '
+								</div>';
+									}
+									$thereis=true;
+							}
+							if($thereis==false){
+								echo '<h4 class="text-danger">No department registered...</h4>';
+							}
+							?>	
+						</div>	
+					</div>
+				</div>
+			</div>
 		</div>
-		<div class="panel-body">
-			<?php
-			$thereis=false;
-			foreach($departments as $d){
-				$dept = str_ireplace(' ','-',$d['department']);
-				echo'
-				<button class="accordion">'.$d['department'].'</button>
-				<div class="panel" id="panel">
-					<p>'.$d['departmentHead'].'<b>(head)</b></p>';
-					foreach($userdata as $us){
-						if($us['department'] == $d['department']){
-							echo '<p>'.$us['firstname'].' '.$us['lastname'].'<b>'.$us['position'].'</b></p>';
-						}
-						echo '
-				</div>';
-					}
-					$thereis=true;
-			}
-			if($thereis==false){
-				echo '<h4 class="text-danger">No department registered...</h4>';
-			}
-			?>	
-		</div>	
-	</div>
+	</div>				
 </div>
 <script type="text/javascript">
 	var acc = document.getElementsByClassName("accordion");
