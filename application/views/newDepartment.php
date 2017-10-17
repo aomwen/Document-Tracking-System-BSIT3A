85106b984f92036752922563943ba760cd30640d
IrlGrace <irlandezmgrace@gmail.com>
2017-10-18 02:26:40 +0800

    New Department Validation

--- application/views/newDepartment.php
+++ application/views/newDepartment.php
@@ -11,11 +11,8 @@
 	}?>
 		<span class="glyphicon glyphicon-circle-arrow-left"></span>&nbspBack </a>Add Department</div>
 		<div class="panel-body">
-		<?php 
-		foreach($colleges as $cl){
 			
-		echo form_open_multipart('AdminOffices/addDepartment/'.$cl['collegeId'].'');}
-		?>
+		<form class="formstyle" method="post" id="department_form">
 		<div class="col-sm-8" style="margin: auto;">
 			<div class="form-group">
 			<?php
@@ -24,10 +21,36 @@
 			</div>
 			<div class="form-group">
 			<label>Department:</label>
-			<input type="text" name="department" placeholder="i.e. Math Department" class="form-control" />
+			<input type="text" name="department" placeholder="i.e. Math Department" class="form-control" id="department"/>
 			</div>
-			<input type="submit" value="Add" class="btn btn-info" />
-			<input type="reset" value="reset" class="btn btn-info" />
+			<div class="pull-right">
+			<input type="submit" value="Add" class="btn btn-primary" />
+			<input type="reset" value="Reset" class="btn btn-primary" />
+			</div>
 		</div>
-	</body>
-</html>
+
+<script>
+ $('#department_form').on('submit',function(e){
+    e.preventDefault();
+    if($('#department').val() != ''){
+    	
+          $.ajax({
+          url:"<?php echo base_url(); ?>AdminOffices/addDepartment/<?php foreach($colleges as $cl){echo $cl['collegeId']; }?>", 
+          method:"POST",
+          data:new FormData(this),
+          contentType:false,
+          cache:false,
+          processData:false,
+          success:function(data){
+      		  alert("Successfully added the department.");
+          }
+
+        });
+        
+      
+  }else{
+        alert("Please Fill in the department name field.");
+  }
+  });
+
+</script>
