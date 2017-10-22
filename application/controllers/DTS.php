57bdeebcfeb4886728e0579863eaf5fa5148f0b7
IrlGrace <irlandezmgrace@gmail.com>
2017-10-22 23:15:39 +0800

    Additional session

--- application/controllers/DTS.php
+++ application/controllers/DTS.php
@@ -5,7 +5,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		public function __construct(){
 			parent::__construct();
 			$this->load->model('registrarDocumentsModel','regDoc');
-}		public function index(){	
+			if(isset($_SESSION['username']))
+	        {
+	            redirect('Dashboard/dashboardview','refresh');
+	        }	
+		}		
+		public function index(){	
 		    $regDocuments = array();
 	        $condition = null;
 	        $rs = $this->regDoc->read($condition);
